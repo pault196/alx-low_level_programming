999de17798228ecb8e89873e29e370fe42128330
pault196 <ogundanapault@gmail.com>
2023-02-20 15:14:30 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
  * different combinations of two digits
  * Numbers must be separated by ,, followed by a space
  * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits0 
+ * 01 and 10 are considered the same combination of the two digits0
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
  * Return: 0
