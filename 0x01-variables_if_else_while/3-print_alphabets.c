00aba4b043a2ed79a7edf1ab4dcf3adee1ca3625
pault196 <ogundanapault@gmail.com>
2023-02-20 15:56:45 -0800

    ./3-print_alphabets | cat -e

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * main - main block
  * Description: Get a random it with 5
- * Return (0)
+ * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z')
+	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
