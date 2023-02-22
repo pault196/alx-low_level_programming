cea5768e20ea342d3f50c17283e937e4f907b027
pault196 <ogundanapault@gmail.com>
2023-02-22 05:39:14 -0800

    ./0-putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the characters c to stdout
+ * @C: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and erro is set appropriately.
+ */
+
+int _putcha(char c)
+{
+
+	return (printf(1, &c, 1));
+}
